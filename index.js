a39b26bf7d7fe352a97b9b9c9eac51d90deba838
quangnh391 <quangnh39@fpt.com>
2024-11-14 18:56:20 +0700

    add

--- index.js
+++ index.js
@@ -2,8 +2,57 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {ScriptManager, Federated, Script} from '@callstack/repack/client';
+
+const containers = {
+  mini: `http://localhost:9000/[name][ext]`,
+};
+
+const isProduct = false;
+ScriptManager.shared.setMaxListeners(1000000000);
+ScriptManager.shared.addResolver(async (scriptId, caller) => {
+  const resolveURL = Federated.createURLResolver({
+    containers,
+  });
+
+  let url;
+  if (!isProduct) {
+    if (__DEV__ && caller === 'main') {
+      url = Script.getDevServerURL(scriptId);
+    } else {
+      url = resolveURL(scriptId, caller);
+    }
+    if (url) {
+      return {
+        url,
+        cache: true,
+        query: {
+          platform: Platform.OS,
+        },
+        verifyScriptSignature: 'off',
+      };
+    }
+  } else {
+    if (caller === 'main') {
+      url = Script.getDevServerURL(scriptId);
+    } else {
+      url = resolveURL(scriptId, caller);
+    }
+
+    if (url) {
+      return {
+        url,
+        cache: true,
+        query: {
+          platform: Platform.OS,
+        },
+        verifyScriptSignature: 'strict',
+      };
+    }
+  }
+});
 
 AppRegistry.registerComponent(appName, () => App);
